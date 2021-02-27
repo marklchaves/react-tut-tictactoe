5ebb3f923f38706f0ba17516a938263071eecc14
mark l chaves <mark@marklchaves.com>
2021-02-27 21:43:16 +0800

    Refactored

--- src/Game.js
+++ src/Game.js
@@ -2,40 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-import { Reset, Square } from "./utils";
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div className="game-table">
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
+import Board from "./Board";
+import { calculateWinner, Reset } from "./utils";
 
 export default class Game extends React.Component {
   state = {
@@ -46,15 +14,15 @@ export default class Game extends React.Component {
 
   resetBoard() {
     return {
-      squares: Array(9).fill(null),
+      squares: Array(9).fill(null)
     };
   }
 
   handleReset() {
-    this.setState({ 
+    this.setState({
       history: [this.resetBoard()],
       xIsNext: true,
-      stepNumber: 0    
+      stepNumber: 0
     });
   }
 
@@ -62,8 +30,7 @@ export default class Game extends React.Component {
     //const history = this.state.history;
     // Move back in history. New moves will erase
     // old history's future moves.
-    const history = 
-      this.state.history.slice(0, this.state.stepNumber + 1);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -82,9 +49,9 @@ export default class Game extends React.Component {
   }
 
   jumpTo(step) {
-    this.setState({ 
-      stepNumber: step, 
-      xIsNext: step % 2 === 0 
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
     });
   }
 
@@ -132,25 +99,3 @@ export default class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
-
-/** Helpers */
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,37 @@
-export const Square = (props) => {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {/* Display X or O in the square. */}
-      {props.value}
-    </button>
-  );
-};
-
-export const Reset = (props) => {
-  return (
-    <button className="reset" onClick={props.onClick}>
-      Reset
-    </button>
-  );
-};
+export const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  
+  export const Reset = (props) => {
+    return (
+      <button className="reset" onClick={props.onClick}>
+        Reset
+      </button>
+    );
+  };
+  
+  export const Square = (props) => {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {/* Display X or O in the square. */}
+        {props.value}
+      </button>
+    );
+  };
+  
